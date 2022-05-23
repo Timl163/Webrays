e47dc3def364aa0d24efd8f0b680851b91167729
Tim Lücking <tim.luecking1@fh-bielefeld.de>
2022-05-23 15:12:33 +0200

    First Commit

---
+++ build/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/git/webrays/googletest/googlemock
+# Build directory: C:/git/webrays/build/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
